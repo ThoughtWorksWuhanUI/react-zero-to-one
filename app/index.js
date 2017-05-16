ca9fcd12184b396dac419f6118f7f446ba45e1ba
benweizhu <benweizhu@gmail.com>
2017-05-16 22:29:35 +0800

    your first react app

--- app/index.js
+++ app/index.js
@@ -1,2 +1,10 @@
-var textnode = document.createTextNode("Hello React 0 to 1");
-document.body.appendChild(textnode);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloMessage extends React.Component {
+  render() {
+    return <div>Hello, {this.props.message}</div>;
+  }
+}
+
+ReactDOM.render(<HelloMessage message="My First React App" />, document.body);
