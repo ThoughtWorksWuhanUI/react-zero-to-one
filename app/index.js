a73ec4b2130882ff44bc89004680afac05043684
benweizhu <benweizhu@gmail.com>
2017-05-26 16:02:10 +0800

    add normalize

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloMessage from './components/HelloMessage';
+import 'normalize.css';
 
 ReactDOM.render(<HelloMessage message="My First React App" />, document.body);
