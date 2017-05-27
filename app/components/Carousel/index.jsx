6f54af66570e787be74e103323a6527784d528df
benweizhu <benweizhu@gmail.com>
2017-05-27 18:19:53 +0800

    add carousel arrow and basic structure

--- app/components/Carousel/index.jsx
+++ app/components/Carousel/index.jsx
@@ -1,25 +1,73 @@
 import React from 'react';
 import Slider from 'react-slick';
+import classNames from 'classnames/bind';
 import styles from './styles.scss';
+
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+
+const cx = classNames.bind(styles);
 
 class Carousel extends React.Component {
   render() {
     var settings = {
-      dots: true,
-      infinite: true,
+      infinite: false,
       speed: 500,
-      slidesToShow: 1,
+      slidesToShow: 5,
       slidesToScroll: 1
     };
     return (
-      <div className='container'>
+      <div className={cx('container', this.props.className)}>
         <Slider {...settings}>
-          <div><img src='http://placekitten.com/g/400/200' /></div>
-          <div><img src='http://placekitten.com/g/400/200' /></div>
-          <div><img src='http://placekitten.com/g/400/200' /></div>
-          <div><img src='http://placekitten.com/g/400/200' /></div>
+          <div>
+            <div className={cx('item')}>
+              <img src='https://a0.muscache.com/im/pictures/b7508c4c-eec0-4857-b218-ecf653cc0736.jpg?aki_policy=poster' />
+              <div>Price</div>
+              <div>Star</div>
+            </div>
+          </div>
+          <div>
+            <div className={cx('item')}>
+              <img src='https://a0.muscache.com/im/pictures/b7508c4c-eec0-4857-b218-ecf653cc0736.jpg?aki_policy=poster' />
+              <div>Price</div>
+              <div>Star</div>
+            </div>
+          </div>
+          <div>
+            <div className={cx('item')}>
+              <img src='https://a0.muscache.com/im/pictures/b7508c4c-eec0-4857-b218-ecf653cc0736.jpg?aki_policy=poster' />
+              <div>Price</div>
+              <div>Star</div>
+            </div>
+          </div>
+          <div>
+            <div className={cx('item')}>
+              <img src='https://a0.muscache.com/im/pictures/b7508c4c-eec0-4857-b218-ecf653cc0736.jpg?aki_policy=poster' />
+              <div>Price</div>
+              <div>Star</div>
+            </div>
+          </div>
+          <div>
+            <div className={cx('item')}>
+              <img src='https://a0.muscache.com/im/pictures/b7508c4c-eec0-4857-b218-ecf653cc0736.jpg?aki_policy=poster' />
+              <div>Price</div>
+              <div>Star</div>
+            </div>
+          </div>
+          <div>
+            <div className={cx('item')}>
+              <img src='https://a0.muscache.com/im/pictures/b7508c4c-eec0-4857-b218-ecf653cc0736.jpg?aki_policy=poster' />
+              <div>Price</div>
+              <div>Star</div>
+            </div>
+          </div>
+          <div>
+            <div className={cx('item')}>
+              <img src='https://a0.muscache.com/im/pictures/b7508c4c-eec0-4857-b218-ecf653cc0736.jpg?aki_policy=poster' />
+              <div>Price</div>
+              <div>Star</div>
+            </div>
+          </div>
         </Slider>
       </div>
     );
