40d5543d24c5d37b31a41112a565a85800208236
benweizhu <benweizhu@gmail.com>
2017-06-03 21:03:52 +0800

    exclude node_modules for babel

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const config = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: 'babel-loader'
       },
       {
