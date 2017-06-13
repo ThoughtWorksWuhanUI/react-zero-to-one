47248df36919cec9afe9fef1c28cc33a7b859001
yycao <yycao@thoughtworks.com>
2017-06-13 18:29:06 +0800

    [refactor] [Jane] remove unused variable in state

--- app/components/SingleGuestSelector/index.jsx
+++ app/components/SingleGuestSelector/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import styles from './styles.scss';
 import classNames from 'classnames/bind';
+
 const cx = classNames.bind(styles);
 const MAX_GUESTS_NUMBER = 16;
 class SingleGuestSelector extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      number: props.number
-    };
   }
 
   subNumber = () => {
@@ -24,30 +22,29 @@ class SingleGuestSelector extends React.Component {
   };
 
   updateNumber = (newNumberGenerator) => {
-    let newNumber = newNumberGenerator(this.state.number);
-    this.setState({ number: newNumber });
+    let newNumber = newNumberGenerator(this.props.number);
     this.props.onChange(newNumber);
   };
 
-
   render() {
-    return <div className={cx('single-guest-selector-wrapper')}>
+    const {number,title,reminder}=this.props;
+    return (<div className={cx('single-guest-selector-wrapper')}>
       <div className={cx('single-guest-selector-left')}>
         <div className={cx('single-guest-selector-title')}>
-          <span>{this.props.title}</span></div>
+          <span>{title}</span></div>
         <div className={cx('single-guest-selector-reminder')}>
-          <span>{this.props.reminder}</span></div>
+          <span>{reminder}</span></div>
       </div>
       <div className={cx('single-guest-selector-right')}>
-        <button className={cx('sub-button', { 'inactive-button': this.state.number <= 0 })} type="button">
+        <button className={cx('sub-button', { 'inactive-button':number <= 0 })} type="button">
           <i className={cx('sub-button-icon')} onClick={this.subNumber}></i>
         </button>
-        <div className={cx('guest-number')}>{this.state.number}</div>
-        <button className={cx('plus-button', { 'inactive-button': this.state.number >= 16 })} type="button">
+        <div className={cx('guest-number')}>{number}</div>
+        <button className={cx('plus-button', { 'inactive-button': number >= 16 })} type="button">
           <i className={cx('plus-button-icon')} onClick={this.plusNumber}></i>
         </button>
       </div>
-    </div>
+    </div>);
   }
 }
 
