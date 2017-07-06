b876854295fd2c55a66977e8699ee2e117c716ce
benweizhu <benweizhu@gmail.com>
2017-07-06 12:05:58 +0800

    add basic router

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware,combineReducers } from 'redux';
-import moment from 'moment';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import thunk from 'redux-thunk';
+import moment from 'moment';
 import HomePage from './HomePage';
+import SearchResultPage from './SearchResultPage';
 import searchCriteria from '../redux/reducer/SearchCriteria';
 import user from '../redux/reducer/user';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const store = createStoreWithMiddleware(combineReducers({searchCriteria,user}));
+const store = createStoreWithMiddleware(combineReducers({ searchCriteria, user }));
 
+moment.locale('zh-cn');
 
 class App extends React.Component {
   render() {
-    moment.locale('zh-cn');
     return (
-      <Provider store={ store }>
-        <HomePage/>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/search">About</Link></li>
+            </ul>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/search" component={SearchResultPage} />
+          </div>
+        </Router>
       </Provider>
     )
   }
--- app/containers/SearchResultPage/index.jsx
+++ app/containers/SearchResultPage/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchResultPage extends React.Component {
   render() {
     return (
-      <div></div>
+      <div>SearchResultPage</div>
     );
   }
 }
